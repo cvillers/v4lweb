df54e3f4a5f7f4f7e05b1ac6a215739ede17ec5a
Cameron Villers <cameron@ville.rs>
2012-02-19 03:59:09 -0500

    Added route names to config, this can make the page controller more generic

--- controllers/page.js
+++ controllers/page.js
@@ -2,10 +2,21 @@ var spawner = require("../lib/spawner");
 
 module.exports = function(app) {
 
-app.get("/:page", function(req, res) {
-	res.render(req.params.page, { title: config.pages[req.params.page].title, servers: config.servers });
+function pageHandler(req, res) {
+	page = "";
+
+	if(typeof req.params.page == 'undefined')
+		page = "home";
+	else
+		page = req.params.page;
+
+	res.render(page, { title: config.pages[page].title, config: config, staticPageName: page });
 	//res.end();
-});
+}
+
+app.get("/", pageHandler);
+app.get("/:page", pageHandler);
+
 
 //app.get("/about", function(req, res) {
 //	res.render("about", { title: "About", servers: config.servers });
